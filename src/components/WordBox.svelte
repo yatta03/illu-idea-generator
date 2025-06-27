418c59d5a62b6c9f41399147cc6e62f3587d98d1
rei <thogasec@gmail.com>
2025-06-27 15:28:46 +0800

    add color&word component

---
+++ src/components/WordBox.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let { words = [] } = $props();
+</script>
+
+<div style="display: flex;">
+  {#each words as w}
+    <div style="display: flex; flex-direction:column; align-items:center;">
+      <div style="width: 100px; height: 50px;"></div>
+      <p>{w}</p>
+    </div>
+  {/each}
+</div>
